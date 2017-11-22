a5423d385fbcf0ae75b6d011bd49b7bafbe30e43
Lars Schweisthal <lars.schweisthal@gmail.com>
2017-11-22 14:48:26 +0100

    Removed head clutter caused by WordPress 'wp_head()' function

--- functions.php
+++ functions.php
@@ -1,3 +1,4 @@
 <?php
 
 require_once 'functions/title-tag.php';
+require_once 'functions/remove-head-clutter.php';
---
+++ functions/remove-head-clutter.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+Removes all scripts/styles in the head of the document.
+
+https://mfarazali.wordpress.com/2016/04/29/remove-junk-from-head/
+*/
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('wp_head', 'wp_resource_hints', 2);
