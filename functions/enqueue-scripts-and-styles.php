3e7a8e2d1f14ef83424fef10f21f0998ba3ff29a
Lars Schweisthal <lars.schweisthal@gmail.com>
2017-11-27 18:08:37 +0100

    [menu] Added jQuery

--- functions/enqueue-scripts-and-styles.php
+++ functions/enqueue-scripts-and-styles.php
@@ -4,6 +4,15 @@
 Enqueues all scripts and stylesheets
 */
 function lindex_enqueue_scripts() {
+  // basic stylesheet
   wp_enqueue_style('base', get_stylesheet_uri(), array(), '1.0.0', 'all');
+
+  //  custom jquery
+  wp_deregister_script('jquery');
+  wp_register_script('jquery', get_template_directory_uri() . '/js/jquery.min.js', array(), '3.2.1', false);
+  wp_enqueue_script('jquery');
+
+  // menu
+  wp_enqueue_script('menu', get_template_directory_uri() . '/js/menu.js', array('jquery'), '1.0.0', false);
 }
 add_action('wp_enqueue_scripts', 'lindex_enqueue_scripts');
