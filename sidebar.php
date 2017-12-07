2e7142d7d27b7113647bcf77a390f79df50c0413
Lars Schweisthal <lars.schweisthal@gmail.com>
2017-12-07 14:49:25 +0100

    added sidebars for every page (cms pages and front page)

--- sidebar.php
+++ sidebar.php
@@ -1,33 +1,54 @@
 <!-- sidebar.php -->
 <?php
+// if Sidebar option is false, we can leave because we wont show a sidebar
+if (get_theme_mod('display_sidebar') == false) {
+  return;
+}
 
-if (get_theme_mod('display_sidebar') && is_active_sidebar('sidebar-global')) {
+// vars
+global $post;
+$showSidebar = false;
 
-?>
-<aside class="sidebar">
-<?php
+// determine the post id
+$id = $post->ID;
 
+// check if there's a sidebar for the current page
+if (
+  is_active_sidebar('sidebar-global') ||
+  is_active_sidebar('sidebar-page-' . $id) ||
+  (is_front_page() && is_active_sidebar('sidebar-page-homepage'))
+) {
+  $showSidebar = true;
+}
+
+// if we don't have a sidebar, we can leave
+if ($showSidebar == false) {
+  return;
 }
 
 ?>
 
+<aside class="sidebar">
+
 <?php
 
-if (get_theme_mod('display_sidebar') && is_active_sidebar('sidebar-global')) {
+// display the sidebar for the start page (when we are here)
+if (is_front_page() && is_active_sidebar('sidebar-page-homepage')) {
+  dynamic_sidebar('sidebar-page-homepage');
+}
+
+// display the sidebar for this very page
+if (is_active_sidebar('sidebar-page-' . $id)) {
+  dynamic_sidebar('sidebar-page-' . $id);
+}
+
+// display the global sidebar
+if (is_active_sidebar('sidebar-global')) {
   dynamic_sidebar('sidebar-global');
 }
 
 ?>
 
-<?php
-
-if (get_theme_mod('display_sidebar') && is_active_sidebar('sidebar-global')) {
-
-?>
 </aside>
-<?php
 
-}
-
-?>
 <!-- /sidebar.php -->
