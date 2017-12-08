a7b166f7fb4d9e968236a6c4e6c6a5dd773c4ce0
Lars Schweisthal <lars.schweisthal@gmail.com>
2017-12-08 10:37:57 +0100

    merge from master

---
+++ functions/remove-filter-wpautop.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Removes the auto-added paragraphs
+*/
+
+// remove autop from all content and excerpts
+remove_filter('the_content', 'wpautop');
+remove_filter('the_excerpt', 'wpautop');
+
+// remove the autop from widget content
+remove_filter('widget_text_content', 'wpautop');
