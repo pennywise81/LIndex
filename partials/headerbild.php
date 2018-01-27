51c544a71edb1b5f61a5573e06aa341c41136747
Lars Schweisthal <lars.schweisthal@gmail.com>
2018-01-27 18:27:15 +0100

    Breakout überarbeitet

--- partials/headerbild.php
+++ partials/headerbild.php
@@ -3,9 +3,15 @@
 $headerbild = get_field('headerbild', get_the_ID());
 
 if (!empty($headerbild)) {
+  echo '</section>';
+  echo '</main>';
+
   echo '<div class="breakout">';
   echo '  <img src="' . $headerbild . '">';
   echo '</div>';
+
+  echo '<main class="page__main container container--maxwidth container--padded">';
+  echo '<section class="page__content">';
 }
 
 ?>
